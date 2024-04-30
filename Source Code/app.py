32768bf0919e6c27155b38f924ea5251d9eb7566
Deepthij8 <125555682+Deepthij8@users.noreply.github.com>
2024-04-30 21:48:32 +0530

    Add files via upload

---
+++ Source Code/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open("water.pkl", "rb"))
+
+@app.route('/')
+def result():
+    return render_template("front.html")
+
+@app.route('/prediction', methods=['POST', 'GET'])
+def prediction():
+    if request.method == 'POST':
+        features = [float(x) for x in request.form.values()]  # Convert to float instead of int
+        final = [np.array(features)]
+        prediction = model.predict_proba(final)
+        output = '{0:.{1}f}'.format(prediction[0][1], 2)
+        output_float = float(output)
+
+        if output_float > 0.5:
+            return render_template("front.html", pred="Water is safe to drink. Potability: {}".format(output_float))
+        else:
+            return render_template("front.html", pred="Water is unsafe. Do not drink. Potability: {}".format(output_float))
+    else:
+        return render_template("front.html", pred="Invalid request method.")
+
+if __name__ == '__main__':
+    app.run(debug=True)
